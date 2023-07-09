0d7c16a96950985c4d1a7ee6e304c05911dd77d9
richard.wieland <rich.wieland73@gmail.com>
2023-07-09 13:52:08 +0200

    refactorings

---
+++ fdbOps.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/apple/foundationdb/bindings/go/src/fdb/directory"
+)
+
+type kvStore struct {
+	instance fdb.Database
+}
+
+func (db kvStore) initFDB() kvStore {
+	fdb.MustAPIVersion(620)
+	db.instance = fdb.MustOpenDefault()
+	db.instance.Options().SetTransactionTimeout(60000) // 60,000 ms = 1 minute
+	db.instance.Options().SetTransactionRetryLimit(100)
+	return db
+}
+
+func (db kvStore) initDirectory(name string) directory.DirectorySubspace {
+	directory, err := directory.CreateOrOpen(db.instance, []string{name}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return directory
+
+}
