0a0cc9bc978ec517713e9cb9b08c9eb2aaebabec
richard.wieland <rich.wieland73@gmail.com>
2023-07-14 13:12:28 +0200

    add func addRectagle

--- image.go
+++ image.go
@@ -9,4 +9,20 @@ type CustomImage struct {
 	draw.Image
 }
 
+func addRectangle(img CustomImage, rect image.Rectangle) draw.Image {
+	myColor := color.RGBA{255, 0, 255, 255}
 
+	min := rect.Min
+	max := rect.Max
+
+	for i := min.X; i < max.X; i++ {
+		img.Set(i, min.Y, myColor)
+		img.Set(i, max.Y, myColor)
+	}
+
+	for i := min.Y; i <= max.Y; i++ {
+		img.Set(min.X, i, myColor)
+		img.Set(max.X, i, myColor)
+	}
+	return img
+}
