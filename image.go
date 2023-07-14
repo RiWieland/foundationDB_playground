c911c444cdd184330005e52f09aa0eeaedca6e89
richard.wieland <rich.wieland73@gmail.com>
2023-07-14 13:18:45 +0200

    add func for readImg

--- image.go
+++ image.go
@@ -1,15 +1,35 @@
 package main
 
-
 import (
-	"image/draw""
+	"image"
+	"image/color"
+	"image/draw"
+	"log"
+	"os"
 )
 
-type CustomImage struct {
+type customImage struct {
 	draw.Image
 }
 
-func addRectangle(img CustomImage, rect image.Rectangle) draw.Image {
+func readImg(path string) customImage {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img_, _ := drawableRGBImage(f)
+	custImg := customImage{
+		img_,
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return custImg
+}
+
+func addRectangle(img customImage, rect image.Rectangle) draw.Image {
 	myColor := color.RGBA{255, 0, 255, 255}
 
 	min := rect.Min
